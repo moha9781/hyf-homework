913b06ce77113a91f06e66eb473d8597deda6c8c
moha9781 <44623475+moha9781@users.noreply.github.com>
2019-01-21 18:05:37 +0100

    Add files via upload

---
+++ javascript/javascript1/week1/homeworkweek1.js
@@ -0,0 +1,50 @@
+let name = "World!";
+let salan = "Wanaagsan";
+console.log("Hello " + name );
+console.log("Subax " + salan );
+function lambar(){
+    const lamb = Math.round(7.25);
+    console.log(lamb);  
+
+}
+lambar();
+//Empty Array
+const empArray = [];
+console.log(empArray);
+
+//Array
+let myAnimals = ["Lion", "Jeatah", "Horse"]; 
+myAnimals.push('baby pig');
+console.log(myAnimals);
+
+// Strings
+let myString = "this is a test";
+let count = myString.count;
+console.log(myString + ' ' + myString.length);
+//  Declaring Variables 
+let x = ('foo');
+const y = ['foo']
+let m = 6
+const l = "foo";
+
+// Typeoff
+console.log(typeof "Hack your future");
+
+console.log(typeof 20);
+
+console.log(typeof true);
+
+console.log(typeof undefined);
+
+// Calculate Future age
+const yearOfBirth = 1995;
+let futureYear  = 2030;
+let age  = futureYear - yearOfBirth;
+console.log('I will be either ' + age + ' or ' + (age - 1));
+
+
+
+
+
+
+
